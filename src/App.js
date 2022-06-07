c47d991dd9ac2921b26799f8e236bc71a329c9b4
Noor-26 <nk26982698@gmail.com>
2022-06-07 15:33:27 +0600

    login form created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navber from './Components/Shared/Navber/Navber';
 import Dairy from './Components/Dairy/Dairy';
 import AddMemory from './Components/Dairy/AddMemory/AddMemory';
 import SeeMemory from './Components/Dairy/SeeMemory/SeeMemory';
+import Login from './Components/Login/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navber/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/dairy" element={<Dairy/>}>
           <Route index element={<AddMemory/>}/>
           <Route  path="seememory" element={<SeeMemory/>}/>
