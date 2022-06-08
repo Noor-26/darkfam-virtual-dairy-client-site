d8ecf61e511d0f4980381a99f236763f08cd84ef
Noor-26 <nk26982698@gmail.com>
2022-06-08 10:24:16 +0600

    user name and sign out btn shown on nav if the user is logged

--- src/Components/Shared/Navber/Navber.js
+++ src/Components/Shared/Navber/Navber.js
@@ -1,8 +1,16 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineMenuUnfold, AiOutlineMenuFold} from 'react-icons/ai';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import Button from '../Button/Button';
+import { signOut } from 'firebase/auth';
 function Navber() {
     const [open, setOpen] = useState(true)
+    const [user] = useAuthState(auth)
+   const logOut = () => {
+    signOut(auth)
+   }
   return (
     <div class="navbar text-neutral-content ">
   <div class="navbar-start">   
@@ -13,7 +21,12 @@ function Navber() {
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ">
       <li><Link to="/">Home</Link></li>
       <li><Link to="/dairy">Diary</Link></li>
-      <li><Link to="/login">Login</Link></li>
+      {user ? <>
+      <p className='btn btn-ghost' onClick={logOut}>{user.displayName}</p>
+      <button className='btn btn-primary' onClick={logOut}>Sign out</button>
+      </>
+      :<li><Link to="/login">Login</Link></li>
+      }
      
       </ul>
     </div>
@@ -23,7 +36,12 @@ function Navber() {
     <ul class="menu menu-horizontal p-0 pr-5 gap-x-5">
     <li><Link to="/">Home</Link></li>
       <li><Link to="/dairy">Diary</Link></li>
-      <li><Link to="/login">Login</Link></li>
+      {user ? <>
+      <p className='btn btn-ghost' onClick={logOut}>{user.displayName}</p>
+      <button className='btn btn-primary' onClick={logOut}>Sign out</button>
+      </>
+      :<li><Link to="/login">Login</Link></li>
+      }
      
     </ul>
   </div>
