071434953c21edc479cd850ac1863d905390cfef
Noor-26 <nk26982698@gmail.com>
2022-06-08 21:00:12 +0600

    Require auth component created

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Route  path="seememory" element={
           <RequireAuth>
           <SeeMemory/>
-        </RequireAuth>
+        </RequireAuth> 
         }/>
           <Route  path="memory/:id" element={
           <RequireAuth>
