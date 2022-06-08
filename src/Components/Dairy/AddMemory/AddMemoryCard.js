2b7d1fe61718a25f6d17927310884f5a4525d57d
Noor-26 <nk26982698@gmail.com>
2022-06-08 10:58:36 +0600

    got time using momentjs library and got user memories

--- src/Components/Dairy/AddMemory/AddMemoryCard.js
+++ src/Components/Dairy/AddMemory/AddMemoryCard.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
+import auth from '../../../firebase.init';
 import Button from '../../Shared/Button/Button';
 import './AddMemory.css'
+import moment  from 'moment';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function AddMemoryCard() {
     const { register, handleSubmit, formState: { errors }} = useForm();
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+    const currentDay = moment().format('MMM Do YY'); 
+    const [user] = useAuthState(auth)
     const onSubmit = data => {
-        console.log(data)
+      const {title,memory} = data
+      const sendData = {
+        userName:user.displayName,
+        email:user.email,
+        current_time:currentTime,
+        title:title,
+        memory:memory
+      }
+      
+      // sending data to server
+      console.log(sendData)
     }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className=' h-auto mt-10'>
     <div className="form-control w-full min-w-xs">
- <input type="text" placeholder="Date" className="input input-bordered mb-4 w-full max-w-xs "  />
+ <input type="text" placeholder="Date" className="input input-bordered mb-4 w-full max-w-xs "  defaultValue={currentDay} disabled />
  </div>
     <div className="form-control w-full min-w-xs">
    <label className="label">
